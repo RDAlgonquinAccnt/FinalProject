4ba4185ecf3ef9fc3552830bd8a2b07f063d80ab
Riley D <rd167956@hotmail.com>
2021-11-29 20:21:29 -0500

    Finished milestone

--- app/src/main/java/com/example/finalproject/Homepage.java
+++ app/src/main/java/com/example/finalproject/Homepage.java
@@ -2,7 +2,10 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class Homepage extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class Homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+
+        ImageView toFinder = findViewById(R.id.imgfinderbtn);
+        ImageView toFavs = findViewById(R.id.favbtn);
+
+        Intent toFinderPage = new Intent(this, ImageFinder.class);
+        Intent toFavsPage = new Intent(this, FavouritePage.class);
+
+        toFinder.setOnClickListener(click -> startActivity(toFinderPage));
+        toFavs.setOnClickListener(click -> startActivity(toFavsPage));
     }
 }
