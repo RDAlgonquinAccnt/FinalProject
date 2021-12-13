b533a94d7fadcb43b1414941bdedf94bf1951a1f
Riley D <rd167956@hotmail.com>
2021-12-13 16:41:09 -0500

    Shared prefs

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,8 +20,23 @@ public class MainActivity extends AppCompatActivity {
 
         TextView username = (TextView) findViewById(R.id.username);
         TextView password = (TextView) findViewById(R.id.password);
+        SharedPreferences prefs = getSharedPreferences("usernamePreferences", MODE_PRIVATE);
 
         Button loginbtn = (Button) findViewById(R.id.loginbtn);
+
+        String usernamepref, passwordpref ;
+
+        if (prefs.contains("username")&& prefs.contains("password")){
+            usernamepref = prefs.getString("username", "");
+
+            passwordpref = prefs.getString("password", "");
+        } else {
+            usernamepref="";
+            passwordpref="";
+        }
+
+        username.setText(usernamepref);
+        password.setText(passwordpref);
 
         Intent toHomePage = new Intent(this, Homepage.class);
 
@@ -33,5 +50,20 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(toHomePage);
             }
         });
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        // makes a shared preferences and editor variable
+        SharedPreferences prefs = getSharedPreferences("usernamePreferences", Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit = prefs.edit();
+        // gets the view from the ID from the layout file
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+        // stores the data in the preferences
+        edit.putString("username", username.getText().toString());
+        edit.putString("password", password.getText().toString());
+        edit.apply();
     }
 }
